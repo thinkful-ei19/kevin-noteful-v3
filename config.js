88a2fbb8568e6067877567e31757148e52b116f6
midimurphdesigns <midimurphdesigns@gmail.com>
2018-03-21 14:34:05 -0700

    updated config file

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 
 exports.PORT = process.env.PORT || 8080;
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-app';
-exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://dev:dev@ds031203.mlab.com:31203/noteful-app-testing';
